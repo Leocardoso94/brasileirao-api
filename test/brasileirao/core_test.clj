d4b5ade23a09f46e51c5038d22e584d18a6a446a
Leonardo Cardoso <leocardosoti@gmail.com>
2018-07-23 18:37:33 -0300

    writing some tests

--- test/brasileirao/core_test.clj
+++ test/brasileirao/core_test.clj
@@ -2,6 +2,9 @@
   (:require [clojure.test :refer :all]
             [brasileirao.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest get-all-teams
+  (is (= 20 (count (get-table)))))
+
+(deftest correct-goals-difference
+  (let [stats (:stats (first  (get-table)))]
+  (is (= (Integer/parseInt (:goal-difference stats)) (- (Integer/parseInt (:goals-for stats)) (Integer/parseInt (:goals-against stats)))))))
