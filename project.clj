8a89052c9f7455f650f179e8e672386509c3f62a
Leonardo Cardoso <leocardosoti@gmail.com>
2018-07-23 22:21:23 -0300

    change min lein-version

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
 (defproject brasileirao "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :min-lein-version "2.8.1"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [enlive "1.1.6"]
                  [http-kit "2.3.0"]
