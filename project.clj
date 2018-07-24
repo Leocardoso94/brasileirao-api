bf2e04e00586cd8b6d1107d2c55f3adc6af8e258
Leonardo Cardoso <leocardosoti@gmail.com>
2018-07-23 21:54:45 -0300

    update lein version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject brasileirao "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.8.1"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
