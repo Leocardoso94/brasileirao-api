bf02b6797947a92ba86b7982c27f5572aaa75f17
Leonardo Cardoso <leocardosoti@gmail.com>
2018-07-23 22:07:28 -0300

    update http-kit

--- project.clj
+++ project.clj
@@ -6,12 +6,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [enlive "1.1.6"]
-                 [http-kit "2.1.18"]
+                 [http-kit "2.3.0"]
                  [compojure "1.5.1"]
                  [ring/ring-json "0.4.0"]
                  [ring-cors "0.1.12"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler brasileirao.handler/app}
-  :jvm-opts ["--add-modules" "java.xml.bind"]
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                                   [ring/ring-mock "0.3.0"]]}})
